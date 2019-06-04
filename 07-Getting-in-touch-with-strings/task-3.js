90343d020c0f7cf7f0960f2adc4951954eda7aed
Tornike Janashia <t.janashia11@gmail.com>
2019-06-04 18:04:14 +0400

    feat(07-Getting-in-touch-with-strings): Fix task-3

--- 07-Getting-in-touch-with-strings/task-3.js
+++ 07-Getting-in-touch-with-strings/task-3.js
@@ -10,7 +10,7 @@ function truncate(string, maxlength){
 
     if(string.length>maxlength)
     {
-        return string.substring(0,maxlength) + "...";
+        return string.substring(0,maxlength-3) + "...";
     }
 
     return string;
